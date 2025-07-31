0f94cece767f044fa20019d71c7328f01a8bf00d
Ícarosun <icaropereirasun@gmail.com>
2025-07-31 18:59:26 -0400

    add two news pointer

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,16 @@ import { Link, router } from 'expo-router'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import MapView, { Marker } from 'react-native-maps'
+import { SvgXml } from 'react-native-svg';
+
+const getSvg = (value, color) => `
+<svg width="50" height="70" viewBox="0 0 50 70" xmlns="http://www.w3.org/2000/svg">
+  <path d="M25 0C11 0 0 11 0 25c0 17.1 25 45 25 45s25-28 25-45C50 11 39 0 25 0z" fill="white"/>
+  <circle cx="25" cy="25" r="23" fill="${color}"/>
+  <text x="25" y="30" font-size="18" fill="black" text-anchor="middle" font-family="Arial" font-weight="bold">${value}</text>
+</svg>
+`;
+
 
 export default function Page() {
   const insets = useSafeAreaInsets(); 
@@ -51,6 +61,40 @@ export default function Page() {
     }
   }
 
+  function CustomTestMarker({ data }: CustomMapMarkerProps) {
+
+    const interpretation = data?.climatologicalInterpretation
+    const elevation = data?.elevation
+
+    if (interpretation) {
+      return (
+        <View style={styles.markerContainerTest}>
+        <View
+          style={[
+            styles.pinHead,
+            { backgroundColor: interpretationColors[interpretation] },
+          ]}
+        >
+          <Text
+            style={[
+              styles.pinText,
+              {
+                color:
+                  interpretation === '-3' || interpretation === '3'
+                    ? 'white'
+                    : 'black',
+              },
+            ]}
+          >
+            {elevation?.toFixed(1)}
+          </Text>
+        </View>
+            <View style={styles.pinTail} />
+        </View>
+      )
+    }
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -72,12 +116,16 @@ export default function Page() {
             }}
             title={station.name}
             onPress={() => handleOpenStationPage(station)}
+            // anchor={{x: 0.4, y: 0.8}} 
+            anchor={{x: 0.5, y:0.6}}
           >
-            <CustomMapMarker
-              data={recentObservedHydrologicalData.find(
-                (e) => e.station_id === station.id
-              )}
-            />
+            {/* <CustomMapMarker */}
+            {/*   data={recentObservedHydrologicalData.find( */}
+            {/*     (e) => e.station_id === station.id */}
+            {/*   )} */}
+            {/* /> */}
+            {/* <CustomTestMarker data={recentObservedHydrologicalData.find((e) => e.station_id === station.id)} />   */}
+            <SvgXml xml={getSvg(35.1, 'orange')} width="50" height="40" />
           </Marker>
         ))}
       </MapView>
@@ -123,9 +171,10 @@ const styles = StyleSheet.create({
     height: 35,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: '100%',
+    // borderRadius: '100%',
     borderWidth: 0.5,
-    borderColor: 'black'
+    borderColor: 'black',
+    // transform: [{ translateY: 20}]
   },
   labelsContainer: {
     position: 'absolute',
@@ -147,4 +196,31 @@ const styles = StyleSheet.create({
     right: 5,
     opacity: 0.8,
   },
+  markerContainerTest: {
+    alignItems: 'center',
+  },
+  pinHead: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 2,
+  },
+    pinText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  pinTail: {
+    width: 0,
+    height: 0,
+    borderLeftWidth: 6,
+    borderRightWidth: 6,
+    borderTopWidth: 12,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderTopColor: 'white',
+    marginTop: -1,
+  }
 })
