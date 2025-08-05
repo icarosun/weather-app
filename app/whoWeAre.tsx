084d4d8d530c37a3b072a3fc79861168074d1d5d
Ícarosun <icaropereirasun@gmail.com>
2025-08-05 18:40:22 -0400

    Add page whoWeAre

---
+++ app/whoWeAre.tsx
@@ -0,0 +1,66 @@
+import {
+  StyleSheet,
+  ActivityIndicator,
+  ScrollView,
+  SafeAreaView,
+  Image,
+  View,
+} from 'react-native'
+
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import { useEffect, useState } from 'react'
+import { Colors } from '@/constants/Colors'
+import { format } from 'date-fns'
+import { useStationContext } from '@/hooks/useStationContext'
+import { formatNumericalData } from '@/utils/formatData'
+
+export default function WhoWeAre() {
+  return (
+    <ThemedView style={styles.mainContainer}>
+      <ThemedText type="default" style={{textAlign: 'justify'}}>
+        Nós somos o <ThemedText type="defaultSemiBold">LabClim</ThemedText> - 
+        Laboratório de Modelagem do Sistema Climático Terrestre. Nosso objetivo é fornecer uma estrutura computacional robusta a fim de apoiar 
+        a formação de recursos humanos capacitados e qualificados em desenvolvimento nas áreas Ambiental, 
+        Hidrologia, Clima, Variabilidade e Mudanças no Clima.
+      </ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 60,
+    height: 60,
+    resizeMode: 'contain',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  forecastRegisterContainer: {
+    marginBottom: 20,
+  },
+  mainContainer: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 20,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  hydrologicalDataContainer: {
+    marginBottom: 30,
+  },
+  hydrologicalDataTitle: {
+    marginBottom: 20,
+  },
+  innerContainer: {
+    marginLeft: 20,
+    marginTop: 10,
+  },
+})
