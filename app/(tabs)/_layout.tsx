116cf865c01f2e3927671f92ea650f63af97d2a4
Ícarosun <icaropereirasun@gmail.com>
2025-08-20 16:08:22 -0400

    Add transparent statusbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,43 +4,50 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { StatusBar } from 'expo-status-bar';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen 
-        name="index" 
-        options={{ 
-          title: 'Mapa',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'map' : 'map-outline'} color={color} />
-          ),
-        }} 
-      />
-      <Tabs.Screen
-        name="observedDataScreen"
-        options={{
-          title: 'Dados Observados',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="forecastDataScreen"
-        options={{
-          title: 'Previsão',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <>
+      <StatusBar translucent backgroundColor="transparent" style="light" />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+          headerShown: false,
+          headerShadowVisible: false,
+          tabBarStyle: {
+          }
+        }}>
+        <Tabs.Screen 
+          name="index" 
+          options={{ 
+            title: 'Mapa',
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon name={focused ? 'map' : 'map-outline'} color={color} />
+            ),
+          }} 
+        />
+        <Tabs.Screen
+          name="observedDataScreen"
+          options={{
+            title: 'Dados Observados',
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="forecastDataScreen"
+          options={{
+            title: 'Previsão',
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
